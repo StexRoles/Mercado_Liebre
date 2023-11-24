04bdaf60da6fbbfda8793521df9b5a76d557f942
StexRoles <navarro456123@gmail.com>
2023-11-23 19:43:38 -0500

    subiendo cambios

--- src/app.js
+++ src/app.js
@@ -6,8 +6,9 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, "../public");
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log("Servidor ejecutandose en http://localhost:3000/");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Servidor ejecutandose en http://localhost:' + port);
 });
 
 
